be412a17b97dba3aa4311464d5fd5f1e8f12e3ec
László Bátori <laszlo.batori@icellmobilsoft.hu>
2024-08-05 14:01:19 +0200

    feat(em): letter spacing comes from `em` units

--- utils/shared.mjs
+++ utils/shared.mjs
@@ -5,7 +5,7 @@ export function setPropValue(obj, propPath, value) {
   const [propName, ...restPath] = propPath;
 
   if (restPath.length > 0) {
-    setPropValue(obj[propName] = obj[propName] || {}, restPath, value);
+    setPropValue((obj[propName] = obj[propName] || {}), restPath, value);
   } else {
     obj[propName] = value;
   }
@@ -14,32 +14,31 @@ export function setPropValue(obj, propPath, value) {
 }
 
 export function getPropValue(obj, propPath) {
-  return propPath.reduce((acc, propName) => (acc?.[propName]), obj);
+  return propPath.reduce((acc, propName) => acc?.[propName], obj);
 }
 
 export function isValueRef(value) {
-  return typeof value === "string" && valueRefRegExp.test(value);
+  return typeof value === 'string' && valueRefRegExp.test(value);
 }
 
 export function getValueRefPath(value) {
-  return valueRefRegExp.exec(value)[1].split(".");
+  return valueRefRegExp.exec(value)[1].split('.');
 }
 
 export function hasModeExtensions(obj) {
   const modes = obj.$extensions.mode;
-  return (
-    modes != null && typeof modes === 'object' && Object.keys(modes).length > 0
-  );
+  return modes != null && typeof modes === 'object' && Object.keys(modes).length > 0;
 }
 
 export function roundDecimals(value, decimal) {
   return +parseFloat(value).toFixed(decimal);
 }
 
-export function fixUnit(obj, correctUnit) {
+export function fixUnit(obj, correctUnit, transformValue = (val) => val) {
   Object.keys(obj).forEach((propName) => {
     const tokenObj = obj[propName];
-    const normalizedTokenValue = roundDecimals(tokenObj.value, CSS_MAX_DECIMAL_PRECISION);
-    tokenObj.value = `${normalizedTokenValue}${correctUnit}`
+    const modifiedTokenValue = transformValue(parseFloat(tokenObj.value));
+    const normalizedTokenValue = roundDecimals(modifiedTokenValue, CSS_MAX_DECIMAL_PRECISION);
+    tokenObj.value = `${normalizedTokenValue}${correctUnit}`;
   });
 }
